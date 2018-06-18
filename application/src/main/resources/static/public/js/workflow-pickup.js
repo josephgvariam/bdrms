68b6d9154e2265f46cd7fd84d8cad8fbd1ab599f
joppu <josephgeorgevariam@gmail.com>
2018-06-18 12:01:22 +0530

    added spinner and info modal

--- application/src/main/resources/static/public/js/workflow-pickup.js
+++ application/src/main/resources/static/public/js/workflow-pickup.js
@@ -6,6 +6,33 @@
                 return '/api/' + this.get('type').toLowerCase() + 'requests';
             }
             return '/api/requests';
+        },
+
+        initialize: function(){
+            this.listenTo(this, 'request', this.requestStart);
+            this.listenTo(this, 'sync', this.requestSuccess);
+            this.listenTo(this, 'error', this.requestError);
+            this.spinner = new Spinner();
+        },
+
+        startSpinner: function(){
+            this.spinner.spin(document.getElementById('loading'));
+        },
+
+        stopSpinner: function(e){
+            this.spinner.stop();
+        },
+
+        requestStart: function(model, xhr, options){
+            this.startSpinner();
+        },
+
+        requestSuccess: function(model, response, options){
+            this.stopSpinner();
+        },
+
+        requestError: function(model, response, options){
+            this.stopSpinner();
         }
 
     });
@@ -28,7 +55,6 @@
         handleSave: function(e){
             e.preventDefault();
 
-
             this.$('.form-group').removeClass('has-error has-feedback');
             this.$('.help-block').remove();
 
@@ -36,9 +62,6 @@
                 this.showValidationErrors('userAssigned', 'may not be null');
                 return;
             }
-
-            this.$('.form-group').removeClass('has-error has-feedback');
-            this.$('.help-block').remove();
 
             this.model.save({
                     userAssigned: {id: this.$('#userAssigned').val()},
@@ -55,7 +78,7 @@
         {
             //console.log('success', response);
             //Backbone.history.navigate("/" + response.id, {trigger: true});
-            window.location.href='/requests/' + response.id;
+            this.options.rootView.showInfoModal('Info', 'User has been assigned successfully.', function(){window.location.href='/requests/' + response.id});
         },
 
         handleSaveError: function(model, response){
@@ -82,7 +105,19 @@
         },
 
         showAssignUserView: function (request) {
-            this.showChildView('main', new AssignUserView({model: request}));
+            this.showChildView('main', new AssignUserView({model: request, rootView: this}));
+        },
+
+        showInfoModal: function(title, message, onHideFunction){
+            var modal = $('#infoModal');
+            modal.find('.modal-title').text(title);
+            modal.find('.modal-body').html('<p>' + message + '</p>');
+
+            if(onHideFunction) {
+                modal.on('hidden.bs.modal', onHideFunction);
+            }
+
+            modal.modal();
         }
     });
 
