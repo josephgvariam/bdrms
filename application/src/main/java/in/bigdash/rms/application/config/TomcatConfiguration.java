3bf370cfb460569527f7b7fdd622732f377803e3
joppu <josephgeorgevariam@gmail.com>
2018-07-07 09:02:35 +0530

    log tomcat path

---
+++ application/src/main/java/in/bigdash/rms/application/config/TomcatConfiguration.java
@@ -0,0 +1,59 @@
+package in.bigdash.rms.application.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.session.StandardManager;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfiguration {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+
+
+        tomcat.addContextCustomizers(new TomcatContextCustomizer() {
+            @Override
+            public void customize(Context context) {
+                if (context.getManager() instanceof StandardManager) {
+                    log.debug("TOMCAT SESSIONS AT: {}",((StandardManager) context.getManager()).getPathname());
+                }
+            }
+        });
+
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+
+        return connector;
+    }
+}
