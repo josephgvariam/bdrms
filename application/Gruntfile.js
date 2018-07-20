9b7b02f36059aa07d1a76e86769bd7e450aef4ab
joppu <josephgeorgevariam@gmail.com>
2018-07-20 12:25:10 +0530

    minified js

--- application/Gruntfile.js
+++ application/Gruntfile.js
@@ -22,27 +22,41 @@ module.exports = function(grunt) {
                 }
             }
         },
-        uglifyPickup: {
-            build: {
-                src: 'src/main/resources/static/public/js/workflow-pickup.js',
-                dest: 'src/main/resources/static/public/js/workflow-pickup.min.js'
-            }
-        },
-        revPickup: {
-            assets: {
-                files: [{
-                    src: [
-                        'src/main/resources/static/public/js/workflow-pickup.min.js',
-                    ]
-                }]
-            }
-        },
         cssmin : {
             target : {
                 src : ['src/main/resources/static/public/css/sweetalert.css'],
                 dest : 'src/main/resources/static/public/css/sweetalert.min.css'
             }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'src/main/resources/static/public/js/workflow-destruction.min.js': 'src/main/resources/static/public/js/workflow-destruction.js',
+                    'src/main/resources/static/public/js/workflow-insertion.min.js': 'src/main/resources/static/public/js/workflow-insertion.js',
+                    'src/main/resources/static/public/js/workflow-permout.min.js': 'src/main/resources/static/public/js/workflow-permout.js',
+                    'src/main/resources/static/public/js/workflow-pickup.min.js': 'src/main/resources/static/public/js/workflow-pickup.js',
+                    'src/main/resources/static/public/js/workflow-refiling.min.js': 'src/main/resources/static/public/js/workflow-refiling.js',
+                    'src/main/resources/static/public/js/workflow-retrieval.min.js': 'src/main/resources/static/public/js/workflow-retrieval.js',
+                    'src/main/resources/static/public/js/workflow-transfer.min.js': 'src/main/resources/static/public/js/workflow-transfer.js',
+                    'src/main/resources/static/public/js/request.min.js': 'src/main/resources/static/public/js/request.js'
+                }
+            }
+        },
+        rev: {
+            files: {
+                src: [
+                    'src/main/resources/static/public/js/workflow-destruction.min.js',
+                    'src/main/resources/static/public/js/workflow-insertion.min.js',
+                    'src/main/resources/static/public/js/workflow-permout.min.js',
+                    'src/main/resources/static/public/js/workflow-pickup.min.js',
+                    'src/main/resources/static/public/js/workflow-refiling.min.js',
+                    'src/main/resources/static/public/js/workflow-retrieval.min.js',
+                    'src/main/resources/static/public/js/workflow-transfer.min.js',
+                    'src/main/resources/static/public/js/request.min.js'
+                ]
+            }
         }
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -53,8 +67,8 @@ module.exports = function(grunt) {
     //grunt.loadNpmTasks('grunt-filerev-replace');
     // see https://martinsonesson.wordpress.com/tag/filerev/
 
-    //grunt.registerTask('default', ['jshint', 'uglifyPickup', 'revPickup']);
     grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'uglify', 'rev']);
     //grunt.registerTask('default', ['cssmin']);
 
 
